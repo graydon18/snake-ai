29ee5fb17fea8d3932521df71858b38119b25ce1
graydon18 <graydol18@gmail.com>
2025-10-24 13:07:24 -0400

    Implement place food function
    Randomize the x,y coordinates of the food and guards againts spawning
    inside the player's existing snake. Add the function to the game init.

--- snakeGame.py
+++ snakeGame.py
@@ -31,7 +31,14 @@ class SnakeGame:
         self.snake = [self.head, Point(self.head.x - GRID_SIZE, self.head.y), Point(self.head.x - (2 * GRID_SIZE), self.head.y)]
         
         self.score = 0
-        self.food = None
+        self.placeFood()
+
+    def placeFood(self):
+        x = random.randint(0, (self.w - GRID_SIZE) // GRID_SIZE) * GRID_SIZE
+        y = random.randint(0, (self.h - GRID_SIZE) // GRID_SIZE) * GRID_SIZE
+        self.food = Point(x, y)
+        if self.food in self.snake:
+            self.placeFood()
 
 game = SnakeGame()
 
