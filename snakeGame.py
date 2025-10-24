6fe35547e99b5c4d430967207dba22ea3e88d141
graydon18 <graydol18@gmail.com>
2025-10-24 12:58:35 -0400

    Initialize snake properties

--- snakeGame.py
+++ snakeGame.py
@@ -26,6 +26,12 @@ class SnakeGame:
         pygame.display.set_caption("Snake AI")
         self.clock = pygame.time.Clock()
 
+        self.direction = Direction.RIGHT
+        self.head = Point(self.w/4, self.h/2)
+        self.snake = [self.head, Point(self.head.x - GRID_SIZE, self.head.y), Point(self.head.x - (2 * GRID_SIZE), self.head.y)]
+        
+        self.score = 0
+        self.food = None
 
 game = SnakeGame()
 
