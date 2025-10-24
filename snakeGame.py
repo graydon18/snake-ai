97b8253b8bd3a574908a7a9fc5eb58db2de732fe
graydon18 <graydol18@gmail.com>
2025-10-24 11:51:14 -0400

    Add direction and movement constants

--- snakeGame.py
+++ snakeGame.py
@@ -1,7 +1,21 @@
 import pygame
 import random
 
+from enum import Enum
+from collections import namedtuple
+
 pygame.init()
+
+class Direction(Enum):
+    UP = 1
+    DOWN = 2
+    LEFT = 3
+    RIGHT = 4
+
+Point = namedtuple('Point', 'x, y')
+
+GRID_SIZE = 20
+VEL = 20
 
 class SnakeGame:
     def __init__(self, w = 960, h = 720):
